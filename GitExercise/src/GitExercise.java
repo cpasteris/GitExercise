b7bc6c5ff7db24b3822847d87259edb2c034269d
cpasteris <cpasteris@shaw.ca>
2020-09-15 15:46:25 -0600

    improved some formatting for feature 1

--- GitExercise/src/GitExercise.java
+++ GitExercise/src/GitExercise.java
@@ -7,11 +7,11 @@ public class GitExercise {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter two numbers that you want to add.");
+		System.out.println("Enter two numbers that you want to add:");
 		int x=sc.nextInt();
 		int y=sc.nextInt();
 		int z=x+y;
-		System.out.println("The two numbers you entered equals: "+z);
+		System.out.println(x+"+"+y+"="+z);
 		sc.close();
 	}
 
