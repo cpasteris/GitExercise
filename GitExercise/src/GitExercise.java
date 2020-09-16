8554bcbf58d7a9b4feca6ed2b80666f48c39c3a2
cpasteris <cpasteris@shaw.ca>
2020-09-15 22:15:22 -0600

    did some minor final editing. could not get the out of bounds input at the beggining figured out in a short enough time

--- GitExercise/src/GitExercise.java
+++ GitExercise/src/GitExercise.java
@@ -21,27 +21,7 @@ public class GitExercise {
 			int y=sc.nextInt();
 			int z=x-y;//subtracts the two numbers
 			System.out.println(x+"-"+y+"="+z);//displays the calculation and the result
-		}else {
-			while(i!=1 || i!=0) {
-				System.out.println("please provide a valid input.\npress 0 if you would like to add two numbers, or 1 if you would like to subtract two numbers.");
-				i = sc.nextInt();
-				if (i==1 || i==0) {break;}
-			}if(i==0) {
-				System.out.println("Enter two numbers that you want to add:");
-				int x=sc.nextInt();//takes to numbers as user input
-				int y=sc.nextInt();
-				int z=x+y;//adds the two numbers together
-				System.out.println(x+"+"+y+"="+z);//displays the calculation and the result
-				}
-			if(i==1) {
-				System.out.println("Enter two numbers that you want to subtract:");
-				int x=sc.nextInt();//takes to numbers as user input
-				int y=sc.nextInt();
-				int z=x-y;//subtracts the two numbers
-				System.out.println(x+"-"+y+"="+z);//displays the calculation and the result
-			}
-		}
+		}else {System.out.println("error, please restart the program.");}
 		sc.close();
 	}
-
 }
